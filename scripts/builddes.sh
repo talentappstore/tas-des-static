008b46870c0ed11335acf4ebcc52a0a267080beb
Andrew <abraae@talentappstore.com>
2014-11-23 12:29:17 +1300

    incorporate shell script and node.js script from tas-core-apis

--- scripts/builddes.sh
+++ scripts/builddes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This script validates the RAML and json schema as much as it can and then generates HTML version of the RAML. Specifically:
+# This script validates the RAML and json schema as much as it can and then generates
+# an index.html file and the HTML versions of the RAML files. Specifically:
 # 1) for all schemas, validate the corresponding example file against the schema
 # 2) for all RAML files, generate the corresponding HTML version
 # 3) generate an overall index.html file
@@ -9,21 +10,28 @@
 # yum install npm
 # npm i -g raml2html tv4
 # export NODE_PATH=/usr/lib/node_modules
-#
 
-# use location of this script to locate our base directory. File layout must be like this:
-# (dir)
-#  |
-#  +- index.html
-#  +- scripts/(this script)
-#  +- raml/
-#  +- generated/
-#  +- schemas/
-#  +- examples/
-DIR="$( cd "$( dirname "$0" )" && pwd )/.."
+# use location of this script to locate other scripts
+SCRIPTDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# use parameter to locate our base directory
+if [ "$#" -ne 1 ]; then
+  echo "Usage: builddes.sh rootdir"
+  echo "where directory layout is like this:"
+  echo "(rootdir)"
+  echo " |"
+  echo " +- index.html"
+  echo " +- raml/"
+  echo " +- generated/"
+  echo " +- schemas/"
+  echo " +- examples/"
+  exit 1
+else
+  DIR=$1  
+fi
 
 echo "validating all schemas"
-$DIR/scripts/validateschemas.js $DIR
+$SCRIPTDIR/validateschemas.js $DIR
 
 echo "generating HTML"
 
@@ -31,7 +39,8 @@ echo "<html><head><title>TAS core</title>" > $DIR/index.html
 echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" >> $DIR/index.html
 echo "<meta name='viewport' content='width=device-width, initial-scale=1'>" >> $DIR/index.html
 echo "</head><body>" >> $DIR/index.html
-echo "<div class='container'><h1>Talent App Store core APIs and schemas</h1>" >> $DIR/index.html
+NAME=`cat $DIR/meta/name`
+echo "<div class='container'><h1>$NAME</h1>" >> $DIR/index.html
 
 echo "<h2>APIs</h2><table class='table table-bordered table-condensed'><thead><tr><th></th><th>HTML</th><th>RAML</th></tr></thead><tbody>" >> $DIR/index.html
 for f in $DIR/raml/*.raml
