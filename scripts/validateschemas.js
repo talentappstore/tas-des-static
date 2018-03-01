52a4af6aaa801b3167add75fd3bdf9d88cafebc6
Andrew Braae <abraae@gmail.com>
2018-03-02 11:05:08 +1300

    updates

--- scripts/validateschemas.js
+++ scripts/validateschemas.js
@@ -7,8 +7,31 @@ tv4.addFormat(formats);
 
 var fs=require('fs');
 var Ajv = require('ajv');
-var ajv = Ajv();
 
+// the following is as per https://github.com/epoberezkin/ajv/releases/tag/5.0.0
+// remove once we've migrated all schemas from draft-04
+//var ajv = Ajv();
+var ajv = new Ajv({
+	  meta: false, // optional, to prevent adding draft-06 meta-schema
+	  extendRefs: true, // optional, current default is to 'fail', spec behaviour is to 'ignore'
+	  unknownFormats: 'ignore',  // optional, current default is true (fail)
+	  // ...
+	});
+
+	var metaSchema = require('ajv/lib/refs/json-schema-draft-04.json');
+	ajv.addMetaSchema(metaSchema);
+	ajv._opts.defaultMeta = metaSchema.id;
+
+	// optional, using unversioned URI is out of spec, see https://github.com/json-schema-org/json-schema-spec/issues/216
+	ajv._refs['http://json-schema.org/schema'] = 'http://json-schema.org/draft-04/schema';
+
+	// Optionally you can also disable keywords defined in draft-06
+	ajv.removeKeyword('propertyNames');
+	ajv.removeKeyword('contains');
+	ajv.removeKeyword('const');
+
+	
+	
 var args = process.argv.slice(2);
 
 var dir = args[0];
